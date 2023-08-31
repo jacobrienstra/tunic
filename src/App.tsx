fce394deca08dac76355276e8073063bb8fbc41c
Jacob Rienstra <jacobdrienstra@gmail.com>
2023-08-31 17:30:16 -0400

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Glyph from './components/Glyph'
+import { css } from '@emotion/react'
+
+
+function App() {
+
+  return (
+    <div>
+      <Glyph />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Glyph.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Glyph() {
+  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 200" fill="black"></svg>
+}
+
+export default Glyph
